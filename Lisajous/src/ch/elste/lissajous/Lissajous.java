751cf5d85d539c90a05826a64d5bbcf2f87d5604
Dillon <Dillon@DillonsPC>
2019-01-09 22:57:39 +0100

    Core Work
    Lissajous.java - 0.3;1.0
    LissajousObject.java - 0.3;1.0

--- Lisajous/src/ch/elste/lissajous/Lissajous.java
+++ Lisajous/src/ch/elste/lissajous/Lissajous.java
@@ -5,8 +5,12 @@ import java.awt.Graphics2D;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
@@ -14,75 +18,107 @@ import javax.swing.SwingWorker;
 import ch.elste.lissajous.LissajousObject.LissajousFunction;
 
 /**
- * @version 0.2
+ * An uninstancable class to start the program.
+ * 
+ * @version 1.0
  * @author Dillon
  *
  */
 public class Lissajous implements Runnable {
-	public static boolean rendering = true;
+	public static final int HEIGHT = 800;
+	public static final int WIDTH = 800;
+	public static final boolean ON_SCREEN = true;
 	public static final double FREQUENCY = 1d / 2d;
-	public static final int CIRCLE_RADIUS = 80;
+	public static final double PRECISION = 0.001d;
+	public static final int CIRCLE_DISTANCE = 50;
+	public static final int OFFSET = 5;
+	public static final int CIRCLE_RADIUS = CIRCLE_DISTANCE / 2 - OFFSET;
+	public static final String FORMAT = "jpg";
+	public static final File SAVE_FILE = new File(System.getProperty("user.home") + "\\Desktop\\lissajous." + FORMAT);
 
 	private final int circlesX, circlesY;
 	private static final Lissajous instance = new Lissajous();
+	private static boolean active = true;
+
+	private static JPanel panel;
 	private static long frameStartTime, frameTime;
 	private BufferedImage currFrame;
-	private JPanel panel;
+	private static BufferedImage saveImage;
 	private Graphics2D g2d;
 	private LissajousObject[] xCircles, yCircles;
+	private LissajousObject[][] circles;
 	private double theta;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(instance);
 	}
 
-	public Lissajous() {
+	/**
+	 * @since 0.2
+	 */
+	private Lissajous() {
 		JFrame frame = new JFrame("Lissajous");
 		panel = new JPanel();
 
-		panel.setPreferredSize(new java.awt.Dimension(800, 800));
+		panel.setPreferredSize(new java.awt.Dimension(WIDTH, HEIGHT));
+
 		frame.setContentPane(panel);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setVisible(ON_SCREEN);
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				super.windowClosing(e);
-				rendering = false;
+				active = false;
 				System.exit(0);
 			}
 		});
 
-		circlesX = panel.getWidth() / (2 * CIRCLE_RADIUS) - 1;
-		circlesY = panel.getHeight() / (2 * CIRCLE_RADIUS) - 1;
-
-		xCircles = new LissajousObject[circlesX];
-		yCircles = new LissajousObject[circlesY];
-		for (int i = 0; i < circlesX; i++) {
-			xCircles[i] = new LissajousObject((i + 1) * 2 * CIRCLE_RADIUS, 0, new LissajousFunction(i + 1, i + 1));
-			xCircles[i].addAllPoints();
-		}
-		for (int i = 0; i < circlesY; i++) {
-			yCircles[i] = new LissajousObject(0, (i + 1) * 2 * CIRCLE_RADIUS, new LissajousFunction(i + 1, i + 1));
-			yCircles[i].addAllPoints();
-		}
+		circlesX = WIDTH / (CIRCLE_DISTANCE) - 1;
+		circlesY = HEIGHT / (CIRCLE_DISTANCE) - 1;
 	}
 
+	/**
+	 * @since 0.2
+	 */
 	@Override
 	public void run() {
-		currFrame = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
+		currFrame = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+		initPoints();
+
 		SwingWorker<Void, Void> renderThread = new SwingWorker<Void, Void>() {
 
 			@Override
 			protected Void doInBackground() throws Exception {
-				while (rendering) {
+				while (active && ON_SCREEN) {
+
 					frameStartTime = System.nanoTime();
-					render();
-					if (theta < 2 * Math.PI)
+
+					if (theta < 2 * Math.PI) {
+						render(false);
 						theta += FREQUENCY * getFrameTime();
-					else
+					} else if (theta < 3 * Math.PI) {
+						renderFPS(true);
+						theta += FREQUENCY * getFrameTime();
+					} else {
 						theta = 0;
+						render(true);
+						saveImage = deepCopy(currFrame);
+						for (int i = 0; i < circles.length; i++) {
+							for (int j = 0; j < circles[i].length; j++) {
+								circles[i][j].clearAllPoints();
+							}
+						}
+						theta = 0;
+						save();
+					}
+
+					if (ON_SCREEN)
+						panel.getGraphics().drawImage(currFrame, 0, 0, panel);
+
 					frameTime = System.nanoTime() - frameStartTime;
 					// System.out.println(frameTime + "\t" + getFPS());
 				}
@@ -94,11 +130,93 @@ public class Lissajous implements Runnable {
 	}
 
 	/**
+	 * Renders the current frame and draws it to the screen.
+	 * 
+	 * @param clean
+	 *            <li>if true, the horizontal and vertical lines and points are not
+	 *            drawn</li>
+	 *            <li>if false, the horizontal and vertical lines and points are
+	 *            drawn</li>
+	 * 
+	 * @since 0.1
+	 */
+	public void render(boolean clean) {
+		g2d = currFrame.createGraphics();
+
+		g2d.clearRect(0, 0, currFrame.getWidth(), currFrame.getHeight());
+		g2d.setBackground(Color.BLACK);
+		for (int i = 0; i < xCircles.length; i++) {
+			g2d.setColor(Color.getHSBColor(i / (xCircles.length - 1f), 1f, 1));
+
+			xCircles[i].render(g2d);
+
+			if (!clean) {
+				g2d.setColor(Color.WHITE);
+				xCircles[i].renderPoint(g2d, theta);
+
+				g2d.setColor(
+						new Color(g2d.getColor().getRed(), g2d.getColor().getGreen(), g2d.getColor().getBlue(), 50));
+				xCircles[i].renderVerticalLine(g2d, theta);
+				g2d.setColor(Color.WHITE);
+			}
+		}
+		for (int i = 0; i < yCircles.length; i++) {
+			g2d.setColor(Color.getHSBColor(i / (yCircles.length - 1f), 1f, 1));
+
+			yCircles[i].render(g2d);
+
+			if (!clean) {
+				g2d.setColor(Color.WHITE);
+				yCircles[i].renderPoint(g2d, theta);
+
+				g2d.setColor(
+						new Color(g2d.getColor().getRed(), g2d.getColor().getGreen(), g2d.getColor().getBlue(), 50));
+				yCircles[i].renderHorizontalLine(g2d, theta);
+				g2d.setColor(Color.WHITE);
+			}
+		}
+		for (int i = 0; i < circles.length; i++) {
+			for (int j = 0; j < circles[i].length; j++) {
+				g2d.setColor(Color.getHSBColor(i / (circles.length - 1f) + j / (circles[i].length - 1f), 1f, 1));
+
+				circles[i][j].addPoint(theta);
+				circles[i][j].render(g2d);
+
+				if (!clean) {
+					g2d.setColor(Color.WHITE);
+					circles[i][j].renderPoint(g2d, theta);
+				}
+			}
+		}
+
+		if (!clean)
+			renderFPS(false);
+	}
+
+	/**
+	 * Renders the FPS counter to the frame.
+	 * 
+	 * @param infinity
+	 *            <li>if true, infinity is rendered</li>
+	 *            <li>if false, the actual FPS is rendered</li>
+	 * 
+	 * @since 0.3
+	 */
+	private void renderFPS(boolean infinity) {
+		g2d.setColor(Color.BLACK);
+		g2d.fillRect(0, 0, CIRCLE_DISTANCE, CIRCLE_DISTANCE);
+		g2d.setColor(Color.WHITE);
+		g2d.drawString(String.format("FPS: %5.2f", !infinity ? getFPS() : Double.POSITIVE_INFINITY), 5, 20);
+	}
+
+	/**
 	 * Returns the time it took to render the last frame in nanoseconds.
 	 * 
 	 * @return the last frameTime in nanoseconds
 	 * 
 	 * @see #getFrameTime()
+	 * 
+	 * @since 0.2
 	 */
 	public static long getFrameTimeNanos() {
 		return frameTime;
@@ -114,36 +232,120 @@ public class Lissajous implements Runnable {
 	 * @return the last frameTime in seconds
 	 * 
 	 * @see #getFrameTimeNanos()
+	 * 
+	 * @since 0.1
 	 */
 	public static double getFrameTime() {
 		return frameTime / Math.pow(10, 9);
 	}
 
+	/**
+	 * Returns the number of frames calculated in the next frame, if all take the
+	 * same time as the last one.
+	 * 
+	 * @return the number of frames in one second
+	 * @since 0.1
+	 */
 	public static double getFPS() {
 		return 1d / (frameTime / Math.pow(10, 9));
 	}
 
-	public static synchronized boolean isRendering() {
-		return rendering;
+	/**
+	 * A boolean to check if the window is rendering at the moment.
+	 * 
+	 * @return
+	 *         <li>true if the window is rendering</li>
+	 *         <li>false if the window is not rendering</li>
+	 * 
+	 * @since 0.2
+	 */
+	public static synchronized boolean isActive() {
+		return active;
 	}
 
-	public void render() {
-		g2d = currFrame.createGraphics();
-
-		g2d.clearRect(0, 0, currFrame.getWidth(), currFrame.getHeight());
-		g2d.setBackground(Color.BLACK);
-		for (LissajousObject lo : xCircles) {
-			lo.render(g2d);
-			lo.renderPoint(g2d, theta);
-		}
-		for (LissajousObject lo : yCircles) {
-			lo.render(g2d);
-			lo.renderPoint(g2d, theta);
-		}
-
-		g2d.drawString(String.format("FPS: %5.2f", getFPS()), 5, 20);
-
-		panel.getGraphics().drawImage(currFrame, 0, 0, panel);
+	/**
+	 * Returns the main {@link JPanel} drawing to the screen.
+	 * 
+	 * @return the {@code contentPane} of the window
+	 * 
+	 * @since 0.3
+	 */
+	public static JPanel getPanel() {
+		return panel;
 	}
 
+	/**
+	 * A helper method for the saving algorithm.
+	 * 
+	 * @param source
+	 *            the {@link BufferedImage} to be copied
+	 * 
+	 * @return the independent copy of {@code source}
+	 * 
+	 * @since 1.0
+	 */
+	public static BufferedImage deepCopy(BufferedImage source) {
+		BufferedImage copy = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+		copy.createGraphics().drawImage(source, 0, 0, null);
+		return copy;
+	}
+
+	/**
+	 * Saves the current frame to the screen.
+	 * 
+	 * @since 1.0
+	 */
+	public static void save() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					ImageIO.write(saveImage, FORMAT, SAVE_FILE);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				if (!ON_SCREEN) {
+					JOptionPane.showMessageDialog(null, "File saved on Desktop as \"lissajous."+FORMAT+"\"", "Done", JOptionPane.INFORMATION_MESSAGE);
+					System.exit(0);
+				}
+			}
+		}).start();
+	}
+
+	/**
+	 * @since 1.0
+	 */
+	private void initPoints() {
+		xCircles = new LissajousObject[circlesX];
+		yCircles = new LissajousObject[circlesY];
+		for (int i = 0; i < circlesX; i++) {
+			xCircles[i] = new LissajousObject((i + 1) * CIRCLE_DISTANCE + OFFSET, OFFSET,
+					new LissajousFunction(i + 1, i + 1));
+			xCircles[i].addAllPoints();
+		}
+		for (int i = 0; i < circlesY; i++) {
+			yCircles[i] = new LissajousObject(OFFSET, (i + 1) * CIRCLE_DISTANCE + OFFSET,
+					new LissajousFunction(i + 1, i + 1));
+			yCircles[i].addAllPoints();
+		}
+
+		circles = new LissajousObject[circlesX][circlesY];
+		for (int i = 0; i < circles.length; i++) {
+			for (int j = 0; j < circles[i].length; j++) {
+				circles[i][j] = new LissajousObject((i + 1) * CIRCLE_DISTANCE + OFFSET,
+						(j + 1) * CIRCLE_DISTANCE + OFFSET, new LissajousFunction(i + 1, j + 1));
+
+				if (!ON_SCREEN)
+					circles[i][j].addAllPoints(PRECISION);
+			}
+		}
+
+		if (!ON_SCREEN) {
+			render(true);
+			saveImage = deepCopy(currFrame);
+			save();
+		}
+	}
 }
--- Lisajous/src/ch/elste/lissajous/LissajousObject.java
+++ Lisajous/src/ch/elste/lissajous/LissajousObject.java
@@ -1,7 +1,7 @@
 package ch.elste.lissajous;
 
 import java.awt.Graphics2D;
-import java.awt.Polygon;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
 /**
@@ -9,7 +9,7 @@ import java.awt.geom.Point2D;
  * 
  * @author Dillon
  * 
- * @version 0.1
+ * @version 1.0
  *
  */
 public class LissajousObject {
@@ -17,63 +17,235 @@ public class LissajousObject {
 	 * Left upper corner of this {@link LissajousObject}
 	 */
 	private int x, y;
-	private Polygon poly;
+	private GeneralPath curve;
 	@SuppressWarnings("unused")
 	@Deprecated
 	private Function fOld;
 	private LissajousFunction f;
 
+	/**
+	 * @deprecated
+	 * 
+	 * 			Creates a new {@link LissajousObject} with given values.
+	 * @param x
+	 *            the {@link #x}-coordinate of the left top corner
+	 * @param y
+	 *            the {@link #y}-coordinate of the left top corner
+	 * @param f
+	 *            the function to describe the curve
+	 * 
+	 * @since 0.1
+	 * 
+	 * @see #LissajousObject(int, int, LissajousFunction)
+	 */
 	@Deprecated
 	public LissajousObject(int x, int y, Function f) {
 		this.x = x;
 		this.y = y;
 		this.fOld = f;
 
-		poly = new Polygon();
+		curve = new GeneralPath();
 	}
 
+	/**
+	 * @deprecated
+	 * 
+	 * 			Creates a new {@link LissajousObject} with given values.
+	 * @param x
+	 *            the {@link #x}-coordinate of the left top corner
+	 * @param y
+	 *            the {@link #y}-coordinate of the left top corner
+	 * @param f
+	 *            the function to describe the curve
+	 * 
+	 * @since 0.1
+	 * 
+	 * @see #LissajousObject(int, int, LissajousFunction)
+	 */
 	@Deprecated
 	public LissajousObject(int x, int y, StandardFunctions f) {
 		this(x, y, f.getF());
 	}
 
+	/**
+	 * Creates a new {@link LissajousObject} with given values.
+	 * 
+	 * @param x
+	 *            the {@link #x}-coordinate of the left top corner
+	 * @param y
+	 *            the {@link #y}-coordinate of the left top corner
+	 * @param f
+	 *            the function to describe the curve
+	 * 
+	 * @since 0.2
+	 * 
+	 * @see #LissajousObject(int, int, LissajousFunction)
+	 */
 	public LissajousObject(int x, int y, LissajousFunction f) {
 		this.x = x;
 		this.y = y;
 		this.f = f;
 
-		poly = new Polygon();
+		curve = new GeneralPath();
+		curve.moveTo(getX(0d), getY(0d));
 	}
 
+	/**
+	 * Add a single point with angle {@code theta} to the curve.
+	 * 
+	 * @param theta
+	 *            the angle defining the point coordinates in radiant
+	 * 
+	 * @since 0.1
+	 * 
+	 * @see #addAllPoints()
+	 */
 	public void addPoint(double theta) {
-		poly.addPoint(this.x + (int) ((f.f(theta).getX() + 1) * Lissajous.CIRCLE_RADIUS),
-				this.y + (int) ((f.f(theta).getY() + 1) * Lissajous.CIRCLE_RADIUS));
+		curve.lineTo(getX(theta), getY(theta));
 	}
 
+	/**
+	 * Adds all points from 0 to 360 degree. It acts like calling
+	 * {@link #addAllPoints(double) addAllPoints(1)} with a value of 1.
+	 * 
+	 * @since 0.2
+	 */
 	public void addAllPoints() {
 		for (int i = 0; i < 360; i++) {
 			addPoint(i / 180d * Math.PI);
 		}
 	}
 
+	/**
+	 * Adds all the points from 0 to 2*Pi with the given precision.
+	 * 
+	 * @param precision
+	 *            the distance between angles
+	 * 
+	 * @since 1.0
+	 */
+	public void addAllPoints(final double precision) {
+		for (double d = 0; d < 2 * Math.PI; d += precision) {
+			addPoint(d);
+		}
+	}
+
+	/**
+	 * Clears all the points of this curve.
+	 * 
+	 * @since 0.3
+	 * 
+	 * @see #addAllPoints()
+	 */
+	public void clearAllPoints() {
+		curve.reset();
+		curve.moveTo(getX(0d), getY(0d));
+	}
+
+	/**
+	 * Render the curve to the given {@link Graphics2D} object.
+	 * 
+	 * @param g2d
+	 *            the {@link Graphics2D} object to be drawn to
+	 * 
+	 * @since 0.1
+	 */
 	public void render(Graphics2D g2d) {
-		g2d.drawPolygon(poly);
+		g2d.draw(curve);
 	}
 
+	/**
+	 * Render the point on the curve at {@code theta} to the {@link Graphics2D}
+	 * object.
+	 * 
+	 * @param g2d
+	 *            the {@link Graphics2D} object to be drawn to
+	 * @param theta
+	 *            the angle defining the point on the curve
+	 * 
+	 * @since 0.1
+	 */
 	public void renderPoint(Graphics2D g2d, double theta) {
-		g2d.fillOval(this.x + (int) ((f.f(theta).getX() + 1) * Lissajous.CIRCLE_RADIUS) - Lissajous.CIRCLE_RADIUS / 20,
-				this.y + (int) ((f.f(theta).getY() + 1) * Lissajous.CIRCLE_RADIUS) - Lissajous.CIRCLE_RADIUS / 20,
-				Lissajous.CIRCLE_RADIUS / 10, Lissajous.CIRCLE_RADIUS / 10);
+		g2d.fillOval(getX(theta) - Lissajous.CIRCLE_RADIUS / 10, getY(theta) - Lissajous.CIRCLE_RADIUS / 10,
+				Lissajous.CIRCLE_RADIUS / 5, Lissajous.CIRCLE_RADIUS / 5);
 	}
 
+	/**
+	 * Draws a vertical line through the point at {@code theta} to the
+	 * {@link Graphics2D} object.
+	 * 
+	 * @param g2d
+	 *            the {@link Graphics2D} object to be drawn to
+	 * @param theta
+	 *            the angle that defines the x-coordinate of the line
+	 * 
+	 * @since 0.3
+	 */
+	public void renderVerticalLine(Graphics2D g2d, double theta) {
+		g2d.drawLine(getX(theta), 0, getX(theta), Lissajous.getPanel().getHeight());
+	}
+
+	public void renderHorizontalLine(Graphics2D g2d, double theta) {
+		g2d.drawLine(0, getY(theta), Lissajous.getPanel().getWidth(), getY(theta));
+	}
+
+	/**
+	 * Returns the {@link Point2D.Double#x x-coordinate} of the point on this
+	 * {@link LissajousObject} at angle {@code theta}.
+	 * 
+	 * @param theta
+	 *            the angle
+	 * @return x-coordinate in the window of point at angle {@code theta}
+	 * 
+	 * @since 0.3
+	 */
+	public int getX(double theta) {
+		return this.x + (int) ((f.f(theta).getX() + 1) * Lissajous.CIRCLE_RADIUS);
+	}
+
+	/**
+	 * Returns the {@link #x x-coordinate} of this {@link LissajousObject}.
+	 * 
+	 * @return {@link #x x-coordinate}
+	 * 
+	 * @since 0.2
+	 */
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Returns the {@link Point2D.Double#y y-coordinate} of the point on this
+	 * {@link LissajousObject} at angle {@code theta}.
+	 * 
+	 * @param theta
+	 *            the angle
+	 * @return y-coordinate in the window of point at angle {@code theta}
+	 * 
+	 * @since 0.3
+	 */
+	public int getY(double theta) {
+		return this.y + (int) ((f.f(theta).getY() + 1) * Lissajous.CIRCLE_RADIUS);
+	}
+
+	/**
+	 * Returns the {@link #y y-coordinate} of this {@link LissajousObject}.
+	 * 
+	 * @return {@link #y y-coordinate}
+	 * 
+	 * @since 0.2
+	 */
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * A class to hold the equation of the lissajous curves.
+	 * 
+	 * @author Dillon
+	 *
+	 * @since 0.2
+	 */
 	static class LissajousFunction {
 		private int xFactor, yFactor;
 
@@ -88,11 +260,14 @@ public class LissajousObject {
 	}
 
 	/**
-	 * A function defining a Lissajous curve with variable <code>theta</code> in
-	 * radiant.
+	 * @deprecated
+	 * 
+	 * 			A function defining a Lissajous curve with variable {@code theta}
+	 *             in radiant.
 	 * 
 	 * @author Dillon
 	 *
+	 * @since 0.1
 	 */
 	@Deprecated
 	static interface Function {
@@ -101,12 +276,17 @@ public class LissajousObject {
 		 * the Point described by this function.
 		 * 
 		 * @param theta
-		 *              the angle giving the point on the lissajous curve.
+		 *            the angle giving the point on the lissajous curve.
 		 * @return a {@link Point2D.Double} storing a point on this lissajous curve
 		 */
 		public abstract Point2D.Double f(double theta);
 	}
 
+	/**
+	 * @author Dillon
+	 * 
+	 * @since 0.1
+	 */
 	@Deprecated
 	static enum StandardFunctions {
 		CIRCLE(new Function() {
